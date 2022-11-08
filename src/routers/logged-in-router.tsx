116b507a150a56265704ac36078871b19aaa75f4
yugykim <yugyeongk92@gmail.com>
2022-11-08 00:13:43 -0700

    Utilized useReactiveVar for local  field Change

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,11 @@
 import React from "react"
+import { isLoggedInVar } from "../apollo"
 
-export const LoggedIn = () => <span>Logged In</span>
+export const LoggedInRouter = () => {
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+    </div>
+  )
+}
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,12 @@
 import React from "react"
+import { isLoggedInVar } from "../apollo"
 
-export const LoggedOutRouter = () => <span>Logged Out</span>
+export const LoggedOutRouter = () => {
+
+  return (
+    <div>
+      <h1>Log Out</h1>
+      <button onClick={() => isLoggedInVar(true)}>Log In</button>
+    </div>
+  )
+}

ee6ee12e8c32ad56628f540e99e840e84d66a6be
yugykim <yugyeongk92@gmail.com>
2022-12-27 12:55:44 -0700

    Updated Router to V6.4

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,20 +1,18 @@
-
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { SignUp } from "../pages/create-account";
+import React from "react"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Login } from "../pages/login";
+import { SignUp } from "../pages/create-account";
+import { NotFound } from "../pages/404";
+
 
 export const LoggedOutRouter = () => {
   return (
     <Router>
-      <Route>
-        <Route path="/create-account">
-          <SignUp />
-        </Route>
-        <Route path="/">
-          <Login />
-        </Route>
-      </Route>
+      <Routes>
+        <Route path="/create-account" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
     </Router>
   );
 };

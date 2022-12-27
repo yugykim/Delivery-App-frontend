44343b120afcff7a66a719d2e6995279fe0756f5
yugykim <yugyeongk92@gmail.com>
2022-12-26 17:22:20 -0700

    Add LoggedIn / LoggedOutRouter

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,27 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, makeVar } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
 import { LOCAL_STORAGE_TOKEN } from "./constant";
 
 const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token)); //false -> begining
 export const authToken = makeVar(token); //null -> begining
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // set head
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": token || "", //this request will happen everytimes
+    }
+  }
+});
+
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink), //
   cache: new InMemoryCache({
     typePolicies: {
       Query: {

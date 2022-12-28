d78160265d64a29d7ecba20686aa55fb87d9ef60
yugykim <yugyeongk92@gmail.com>
2022-12-27 20:05:37 -0700

    Add Route and replace useQuery to UseMe hook

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,26 +1,21 @@
 import React from "react"
-import { gql, useQuery } from "@apollo/client";
-import { meQuery } from "../gql/meQuery";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Restaurant } from "../pages/client/restaurants";
 import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
+import { ConfirmEmail } from "../pages/user/confirm-email";
+import { EditProfile } from "../pages/user/edit-profile";
 
 const ClientRoutes = [
-  <Route path="/" element={<Restaurant />} />
+  <>
+    <Route key={1} path="/" element={<Restaurant />} />
+    <Route key={2} path="/confrim" element={<ConfirmEmail />} />
+    <Route key={3} path="/edit-profile" element={<EditProfile />} />
+  </>
 ];
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
 export const LoggedInRouter = () => {
-  const {data, loading, error} = useQuery<meQuery>(ME_QUERY);
+  const {data, loading, error} = useMe(); //go to cache
   if ( loading || error || !data) {
     return <div className="h-screen flex justify-center items-center">
       <span className="font-medium text-xl tracking-wide">Loading...</span>

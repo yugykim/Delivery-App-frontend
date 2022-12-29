113ce611bcb1e5dafa339ff98cba97aa85bae102
yugykim <yugyeongk92@gmail.com>
2022-12-28 22:33:06 -0700

    MyRestaurant interface type

---
+++ src/gql/myRestaurants.ts
@@ -0,0 +1,25 @@
+export interface myRestaurants_myRestaurants_restaurants_category {
+  __typename: "Category";
+  name: string;
+}
+
+export interface myRestaurants_myRestaurants_restaurants {
+  __typename: "Restaurant";
+  id: number;
+  name: string;
+  coverImg: string;
+  category: myRestaurants_myRestaurants_restaurants_category | null;
+  address: string;
+  isPromoted: boolean;
+}
+
+export interface myRestaurants_myRestaurants {
+  __typename: "MyRestaurantsOutput";
+  ok: boolean;
+  error: string | null;
+  restaurants: myRestaurants_myRestaurants_restaurants[];
+}
+
+export interface myRestaurants {
+  myRestaurants: myRestaurants_myRestaurants;
+}

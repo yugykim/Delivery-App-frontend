bbeedb316a5a7182078585442d8d39119ccc861e
yugykim <yugyeongk92@gmail.com>
2022-12-28 22:32:28 -0700

    Restaurant Page

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,3 +1,46 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
+import { restaurantsPageQuery, restaurantsPageQueryVariables } from '../../gql/restaurantsPageQuery';
 
-export const Restaurant = () => <h1>Restaurant</h1>
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImage
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResult
+      results {
+        id
+        name
+        coverImage
+        category {
+          name 
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
+export const Restaurant = () => {
+  const { data, loading, error } = useQuery<restaurantsPageQuery, restaurantsPageQueryVariables>(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page:1
+      }
+    }
+  });
+  console.log(data);
+  return <h1>Restaurants</h1>
+};

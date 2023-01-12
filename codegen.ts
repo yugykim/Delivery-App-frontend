d83c33b3ee8a9809865e9b4dff2a41af1036f600
yugykim <yugyeongk92@gmail.com>
2023-01-11 22:54:20 -0700

    Changed doucments from *.tsx to *.tsx,ts

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:4000/graphql",
-  documents: "./src/**/*.tsx",
+  documents: "./src/**/*.{tsx,ts}",
   generates: {
     './src/gql/': {
       preset: "client",

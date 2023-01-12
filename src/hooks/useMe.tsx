91cfc7b14c34cb8dc2a2420c13b60a6a6e7e72af
yugykim <yugyeongk92@gmail.com>
2023-01-11 22:52:44 -0700

    Added searchRestaurant type

--- src/hooks/useMe.tsx
+++ src/hooks/useMe.tsx
@@ -3,7 +3,7 @@ import { meQuery } from "../gql/meQuery";
 
 //apollo put this in cache, and give it to us. so it is better using hook, which calls graphql once, and
 //get things from cache
-const ME_QUERY = gql`
+export const ME_QUERY = gql`
   query meQuery {
     me {
       id

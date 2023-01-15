42beb63233897ad209d476255836d3b6ec8519d5
yugykim <yugyeongk92@gmail.com>
2023-01-14 20:51:25 -0700

    Added Search Page route

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,16 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Restaurant } from "../pages/client/restaurants";
+import { Restaurants } from "../pages/client/restaurants";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
+import { Search } from "../pages/client/search";
 
 const ClientRoutes = [
   <>
-    <Route key={1} path="/" element={<Restaurant />} />
+    <Route key={1} path="/" element={<Restaurants />} />
     <Route key={2} path="/confrim" element={<ConfirmEmail />} />
     <Route key={3} path="/edit-profile" element={<EditProfile />} />
+    <Route key={4} path="/search" element={<Search />} />
   </>
 ];
 

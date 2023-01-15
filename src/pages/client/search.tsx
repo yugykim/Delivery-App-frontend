309f678492fda3d3bc1ea96f1b9635f10b381bff
yugykim <yugyeongk92@gmail.com>
2023-01-14 20:51:40 -0700

    Created Search page.

---
+++ src/pages/client/search.tsx
@@ -0,0 +1,54 @@
+import { gql, useLazyQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
+import { useLocation, useNavigate } from "react-router-dom";
+import { searchRestaurant, searchRestaurantVariables } from "../../gql/searchRestaurant";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+
+
+const SEARCH_RESTAURANT = gql`
+  query searchRestaurant($input: SearchRestaurantInput!) {
+    searchRestaurant(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      restaurants {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
+export const Search = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [callQuery, { loading, data, called }] = useLazyQuery<
+    searchRestaurant,
+    searchRestaurantVariables
+  >(SEARCH_RESTAURANT);
+  useEffect(() => {
+    const [_, query] = location.search.split("?term=");
+    if (!query) {
+      return navigate("/");
+    }
+    callQuery({
+      variables: {
+        input: {
+          page: 1,
+          query,
+        },
+      },
+    });
+  }, [navigate, location]);
+  console.log(loading, data, called);
+  return (
+    <div>
+      <Helmet>
+        <title>Search | Nuber Eats</title>
+      </Helmet>
+      <h1>Search page</h1>
+    </div>
+  );
+};

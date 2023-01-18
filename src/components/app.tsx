959f7f189b3a981ed144b1320f4de571f5c38631
yugykim <yugyeongk92@gmail.com>
2023-01-17 21:27:55 -0700

    Moved file to components folder

---
+++ src/components/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "../apollo";
+import { LoggedInRouter } from "../routers/logged-in-router";
+import { LoggedOutRouter } from "../routers/logged-out-router";
+
+export const App = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return (isLoggedInVar()? <LoggedInRouter /> : <LoggedOutRouter />);
+};

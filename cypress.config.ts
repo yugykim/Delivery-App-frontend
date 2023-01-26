72d581bb0bf396791eb99e47619395a6800339cf
yugykim <yugyeongk92@gmail.com>
2023-01-26 16:26:32 -0700

    Installed Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: 'ca4jv5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });

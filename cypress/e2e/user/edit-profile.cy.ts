174dd3871fb53cd345e94c3784020a5d0c76f2f9
yugykim <yugyeongk92@gmail.com>
2023-01-30 22:55:35 -0700

    e2e test Edit profile

--- cypress/e2e/user/edit-profile.cy.ts
+++ cypress/e2e/user/edit-profile.cy.ts
@@ -1,7 +1,16 @@
 describe('Edit Profile', () => {
   const user = cy;
+  beforeEach(() => {
+    user.login("test@gmail.com", "Test123456")
+    user.visit('/')
+  })
   it("can go to /edit-profile using the header", () => {
-    user.login("test@gmail.com", "Test123456");
     user.get('a[href="/edit-profile"]').click();
+    user.title().should('eq', 'Edit Profile | Nuber Eats');
+  });
+  it("can change email", () => {
+    user.visit('/edit-profile')
+    user.findByPlaceholderText(/email/i).clear().type('new@nomadcoders.co')
+    user.findByRole('button').click();
   })
 })

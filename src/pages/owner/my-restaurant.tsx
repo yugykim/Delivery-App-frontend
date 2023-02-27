10d2f4c59b62fb578ed234a95c356c9c2e3b5a7b
yugykim <yugyeongk92@gmail.com>
2023-02-26 21:16:45 -0700

    Created dish component

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { myRestaurant, myRestaurantVariables } from '../../gql/myRestaurant';
+import { Dish } from '../../components/dish';
 
 const MY_RESTAURANT_QUERY = gql`
 	query myRestaurant($input: MyRestaurantInput!) {
@@ -13,14 +14,14 @@ const MY_RESTAURANT_QUERY = gql`
 			error
 			restaurant {
 				...RestaurantParts
-        menu {
-          ...DishParts
-        }
+				menu {
+					...DishParts
+				}
 			}
 		}
 	}
 	${RESTAURANT_FRAGMENT}
-  ${DISH_FRAGMENT}
+	${DISH_FRAGMENT}
 `;
 
 interface IParams {
@@ -39,31 +40,44 @@ export const MyRestaurant = () => {
 			},
 		}
 	);
-  console.log(data);
-  return (
-    <div>
-      <div
-        className="  bg-gray-700  py-28 bg-center bg-cover"
-        style={{
-          backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
-        }}
-      ></div>
-      <div className="container mt-10">
-        <h2 className="text-4xl font-medium mb-10">
-          {data?.myRestaurant.restaurant?.name || "Loading..."}
-        </h2>
-        <Link to={`/restaurants/${id}/add-dish`} className=" mr-8 text-white bg-gray-800 py-3 px-10">
-          Add Dish &rarr;
-        </Link>
-        <Link to={``} className=" text-white bg-lime-700 py-3 px-10">
-          Buy Promotion &rarr;
-        </Link>
-        <div className="mt-10">
-          {data?.myRestaurant.restaurant?.menu.length === 0 ? (
-            <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) : null}
-        </div>
-      </div>
-    </div>
-  );
+	console.log(data);
+	return (
+		<div>
+			<div
+				className='  bg-gray-700  py-28 bg-center bg-cover'
+				style={{
+					backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
+				}}></div>
+			<div className='container mt-10'>
+				<h2 className='text-4xl font-medium mb-10'>
+					{data?.myRestaurant.restaurant?.name || 'Loading...'}
+				</h2>
+				<Link
+					to={`/restaurants/${id}/add-dish`}
+					className=' mr-8 text-white bg-gray-800 py-3 px-10'>
+					Add Dish &rarr;
+				</Link>
+				<Link
+					to={``}
+					className=' text-white bg-lime-700 py-3 px-10'>
+					Buy Promotion &rarr;
+				</Link>
+				<div className='mt-10'>
+					{data?.myRestaurant.restaurant?.menu.length === 0 ? (
+						<h4 className='text-xl mb-5'>Please upload a dish!</h4>
+					) : (
+						<div className='grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10'>
+							{data?.myRestaurant.restaurant?.menu.map((dish) => (
+								<Dish
+									name={dish.name}
+									description={dish.description}
+									price={dish.price}
+								/>
+							))}
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };

0865a9bc8ad54f79a0a640799d1c7e6b68825255
yugykim <yugyeongk92@gmail.com>
2023-04-11 15:55:38 -0600

    changed backend url

--- src/apollo.ts
+++ src/apollo.ts
@@ -13,14 +13,14 @@ export const authToken = makeVar(token); //null -> begining
 //subscription
 const wsLink = new GraphQLWsLink(createClient({
 
-  url: process.env.NODE_ENV === 'production'? 'ws://uber-eats-backend.herokuapp.com':'ws://localhost:4000/graphql',
+  url: process.env.NODE_ENV === 'production'? 'ws://uber-eats-backend.herokuapp.com/graphql':'ws://localhost:4000/graphql',
   connectionParams: {
     'x-jwt': authToken() || "", 
   },
 }));
 
 const httpLink = createHttpLink({
-  uri: process.env.NODE_ENV === 'production'? 'ws://uber-eats-backend.herokuapp.com':'ws://localhost:4000/graphql',
+  uri: process.env.NODE_ENV === 'production'? 'ws://uber-eats-backend.herokuapp.com/graphql':'ws://localhost:4000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
